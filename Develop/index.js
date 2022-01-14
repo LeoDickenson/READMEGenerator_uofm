79976e4178850356b2cc4222c3017b90772db72a
Leo Dickens <dickens.leo@gmail.com>
2022-01-13 18:23:47 -0600

    added questions

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,18 @@
 // TODO: Include packages needed for this application
 
 // TODO: Create an array of questions for user input
-const questions = [];
+// I will console log each question before the user inputs data
+// then I will add the inputted data to the arguments in the readme generator
+const questions = [
+    'What is the title of your project?',
+    'Enter a description of your project:',
+    'Enter any installation instructions for new users:',
+    'Enter any usage information:',
+    'Enter any guidelines for contributors:',
+    'Enter any test instruction:',
+    'What is your GitHub username?',
+    'What is your email address?',
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
